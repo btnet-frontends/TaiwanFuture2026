60cfa066b89bd31a8aa6e811ce9049170ebb5282
annawei <annawei@benet.com.tw>
2026-06-09 16:01:09 +0800

    0609 修改長榮logo

--- src/components/Partner.vue
+++ src/components/Partner.vue
@@ -9,9 +9,24 @@
     <div class="content" v-for="item in logos" :key="item.title">
       <h4 class="title">{{ item.title }}</h4>
       <div class="partner-logo-sec">
-        <a :href="x.link" target="_blank" class="partner-logo" :class="x.name" v-for="x in item.partner" :key="x.name">
-          <img :src="getImageUrl(x.name)" :alt="x.alt" v-if="x.alt">
-        </a>
+        <template v-for="x in item.partner" :key="x.name">
+
+          <!-- 長榮特殊處理 -->
+          <div class="partner-logo EVERGREEN_EVAAIR" v-if="x.name === 'EVERGREEN_EVAAIR'">
+            <a href="https://www.evergreen-marine.com/emc/?lang=zh-tw" target="_blank" class="evergreen-half">
+              <img :src="getImageUrl('EVERGREEN')" alt="長榮海運">
+            </a>
+            <a href="https://www.evaair.com/zh-tw/index.html" target="_blank" class="evaair-half">
+              <img :src="getImageUrl('EVERGREEN')" alt="長榮航空">
+            </a>
+          </div>
+
+          <!-- 其他正常 logo -->
+          <a :href="x.link" target="_blank" class="partner-logo" :class="x.name" v-else>
+            <img :src="getImageUrl(x.name)" :alt="x.alt" v-if="x.alt">
+          </a>
+
+        </template>
       </div>
     </div>
   </section>
@@ -31,4 +46,25 @@ const getImageUrl = (name) => {
 </script>
 
 <style scoped>
+.EVERGREEN_EVAAIR {
+  display: flex;
+}
+
+.evergreen-half,
+.evaair-half {
+  width: 50%;
+  overflow: hidden;
+  display: block;
+}
+
+.evergreen-half img {
+  width: 200%;
+  object-position: left;
+}
+
+.evaair-half img {
+  width: 200%;
+  margin-left: -100%;
+  object-position: right;
+}
 </style>
